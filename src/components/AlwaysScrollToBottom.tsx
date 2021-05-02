dde3924abf45361a47ab5ec46b56709b56a92770
Jander Silva <Jander.silv@outlook.com>
2021-05-02 13:27:27 -0300

    refactor: componentization of AlwaysScrollToBottom

---
+++ src/components/AlwaysScrollToBottom.tsx
@@ -0,0 +1,28 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+
+interface Props {
+  forwardRef: React.MutableRefObject<HTMLDivElement>
+}
+
+const AlwaysScrollToBottom = ({ forwardRef }: Props): JSX.Element => {
+  const [scrollPosition, setScrollPosition] = useState(0)
+
+  const elementRef = useRef<HTMLDivElement>()
+
+  const checkScrolledUp = useCallback(() => {
+    const actualPosition = forwardRef.current.scrollTop
+    console.log(actualPosition, scrollPosition)
+    if (actualPosition < scrollPosition) return true
+    else {
+      setScrollPosition(forwardRef.current.scrollTop)
+      return false
+    }
+  }, [forwardRef, scrollPosition])
+
+  useEffect(() => {
+    if (!checkScrolledUp()) elementRef.current.scrollIntoView()
+  })
+  return <div id="always-scroll-bottom-div" ref={elementRef} />
+}
+
+export default AlwaysScrollToBottom
